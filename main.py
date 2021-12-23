7591a9242ecdd22e6c74c9ab2e1094ef103a1cc8
DILEM Yvan <yvan.dilem.1@ens.etsmtl.ca>
2021-12-23 14:15:13 -0500

    first commit

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ height = 900
 canvas = Canvas(root, width = width, height=height, bg='black')
 canvas.grid(row=0, column=0)
 Planetes = []
-NbPlanetes = 100
+NbPlanetes = 200
 fps = 60
 # Masse = [1.989e30, 3.285e23, 4.867e24, 5.972e24,  6.39e23]#kg
 # Rayon = [20, 0.38*10, 0.9497*10, 1*10, 0.53*10]
@@ -27,7 +27,7 @@ for i in range(0, NbPlanetes):
     posy = random.randrange(0,height)
     vx = random.randrange(-5,5)
     vy = random.randrange(-5,5)
-    r = random.randrange(1,5)
+    r = random.randrange(1,2)
     Planetes.append(planete(np.array([posx, posy], dtype=float), np.array([vx, vy], dtype=float), r))
 T = 0
 Tx = True
@@ -48,23 +48,28 @@ while Tx == True:
                 else :
                     planete2.agglo(planete1)
                     Planetes.remove(planete1)
-        
-
+    
         planete1.pos += planete1.v
         planete1.update_radius()
         canvas.create_oval(planete1.pos[0] - planete1.r, planete1.pos[1] - planete1.r, planete1.pos[0] + planete1.r, planete1.pos[1] + planete1.r, fill = 'yellow')
         planete1.draw_trajectory(canvas, planete1.Tore_boundary(canvas, False))
     Energy += 0.5 * np.linalg.norm(planete1.v)**2 * planete1.m
     ENERGY.append(Energy)
-    canvas.create_text(50, 20, text = "Energy: "+str(round(Energy,2)), fill = 'white')
-    canvas.create_text(50, 50, text = "Time: "+str(round(T/fps,2)), fill = 'white')
+    canvas.create_text(50, 20, text = "Cinetic energy: "+str(round(Energy,2)), fill = 'white', anchor = W)
+    canvas.create_text(50, 50, text = "Time: "+str(round(T/fps,2)), fill = 'white', anchor = W)
+    canvas.create_text(50, 80, text = "Nb planetes: "+str(round(len(Planetes),2)), fill = 'white', anchor = W)
     canvas.update()
     time.sleep(1/fps)
 
-    if T/fps>=10:
-        Tx==False
+    if T/fps>=100 or len(Planetes)==1:
+        Tx=False
+    print(len(Planetes))
 
 
 plt.figure()
-plt.plot([i for i in range(len(ENERGY))],ENERGY)
+plt.plot([i/fps for i in range(len(ENERGY))],ENERGY)
+plt.title('Cinetic energy vs time')
+plt.xlabel('Time (s)')
+plt.ylabel('Cinetic energy')
+plt.grid()
 plt.show()
